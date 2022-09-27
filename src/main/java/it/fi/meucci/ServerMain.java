18528984278535c07c90e36d817c4a6220d7e716
lai2-01 <lai2-01@itismeucci.com>
2022-09-27 10:52:17 +0200

    firstcommit

---
+++ src/main/java/it/fi/meucci/ServerMain.java
@@ -0,0 +1,12 @@
+package it.fi.meucci;
+
+public class ServerMain {
+    public static void main (String args[]){
+        ServerStr servente =new ServerStr();
+        servente.attendi();
+        servente.comunica();
+        
+
+    }
+    
+}
---
+++ src/main/java/it/fi/meucci/ServerStr.java
@@ -0,0 +1,42 @@
+package it.fi.meucci;
+
+import java.io.*;
+import java.net.*;
+import java.net.*;
+import java.util.*;
+
+public class ServerStr {
+    ServerSocket server = null;
+    Socket client = null;
+    String stringRicevuta = null;
+    String stringModifica = null;
+    BufferedReader inDalClient;
+    DataOutputStream outVersoClient;
+
+    public Socket attendi() {
+        try {
+            System.out.print("server in esecuzione");
+            server = new ServerSocket(6789);
+            client = server.accept();
+            server.close();
+
+            inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            outVersoClient = new DataOutputStream(client.getOutputStream());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("errore");
+            System.exit(1);
+        }
+        return client;
+    }
+
+public void comunica()
+{
+    try
+    {
+        System.out.println("");
+        stringRicevuta =inDalClient.readLine(); 
+        System.out.println(""); 
+      }
+}
+}
